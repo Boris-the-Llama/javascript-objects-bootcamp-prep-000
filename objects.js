b79448f041209e9b2275c4d100eb2b900912f4e3
Dan Brett <boristhellama99@gmail.com>
2018-01-10 19:13:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { 'Rihanna': 'Whats my name?' };
 
 function updatePlaylist(object, artist, song) {
-  return Object.assign({}, object, {[artist]: song});
+  return Object.assign(object, {[artist]: song});
 }
 
 function removeFromPlaylist(object, artist) {
